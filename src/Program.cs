bc050d1ddb2b5dadc130b69be8a3c2b21c11d1d0
ibragimovdev7 <89067487+ibragimovdev7@users.noreply.github.com>
2024-11-07 11:51:34 +0500

    One example solved

--- src/Program.cs
+++ src/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write(" 4 xonali sonni kiriting : ");
+            var value = Convert.ToInt32(Console.ReadLine());
+            var result = 0;
+            for (var i = 0; i < 5; i++)
+            {
+                result += value % 10;
+                value /= 10;
+            }
+            Console.WriteLine("Result " + result);
         }
     }
 }
